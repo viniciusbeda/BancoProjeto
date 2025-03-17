8daa9e1b89d0a5f55ce2a4323fe854aacf463599
Vinicius Beda <vinicius.beda05@gmail.com>
2025-03-17 20:10:17 -0300

    Finalizando Projeto

--- BancoProjeto/BancoConsole/Program.cs
+++ BancoProjeto/BancoConsole/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using BankLibrary.Models;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bem-vindo ao Banco .NET!");
+
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite seu CPF: ");
+        string cpf = Console.ReadLine();
+
+        Cliente cliente = new Cliente(nome, cpf);
+        ContaCorrente conta = new ContaCorrente(123456, 1000);
+
+        Console.WriteLine("\nConta criada com sucesso!");
+        cliente.ExibirDados();
+        conta.ExibirSaldo();
+
+        Console.Write("\nDigite o valor do saque(Não use o R$, digite apenas o valor desejado para o saque): ");
+        decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
+        conta.Sacar(valorSaque);
+        conta.ExibirSaldo();
+    }
+}
